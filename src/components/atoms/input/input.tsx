7fdfb5e47632b98d5cd5df9ea6c5d0f89bac8921
OmerDuraker <omer.duraker@bromcom.com>
2020-11-30 17:01:22 +0300

    edit input

--- src/components/atoms/input/input.tsx
+++ src/components/atoms/input/input.tsx
@@ -1,1 +1,13 @@
-// input
+import { Component,  h } from '@stencil/core';    
+
+@Component({
+    tag: 'bcm-input',
+    styleUrl: 'input.scss',
+})
+export class BcmInput {
+    render() {
+        return (
+            <input type="text" />
+        )
+    }
+}
