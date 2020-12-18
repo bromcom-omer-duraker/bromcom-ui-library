0a7fa4d79b309be6172ad585bf356b397935d7cb
OmerDuraker <omer.duraker@bromcom.com>
2020-12-18 12:17:52 +0300

    add radio button and story

---
+++ src/components/molecules/radio/group.tsx
@@ -0,0 +1,121 @@
+import { Component, h, Element, Prop, Event, EventEmitter, Watch, State } from "@stencil/core";
+import cs from 'classnames'
+
+export declare type OptionType = {
+    label: string,
+    value: string | number,
+    disabled?: boolean
+}
+
+@Component({
+    tag: 'bcm-radio-group',
+    styleUrl: 'group.scss',
+    shadow: true
+})
+export class BcmRadioGroup {
+
+    private childRadios: HTMLBcmRadioElement[] = []
+    
+    @Element() host: HTMLElement
+    
+    @Prop({ mutable: true, reflect: true }) defaultValue: string | number
+    @Prop() direction: 'vertical' | 'horizontal' = 'horizontal'
+    @Prop() options: OptionType[] | string
+    @Prop() optionType: 'default' | 'button' = 'default'
+    @Prop() buttonStyle: 'solid' | 'outline' = 'solid'
+    @Prop() size: 'small' | 'medium' | 'large' = 'medium'
+
+    @State() radioOptions: OptionType[] = []
+    @State() usingSlots: boolean = true
+
+    @Event({ eventName: 'bcm-change' }) change: EventEmitter
+
+    @Watch('options')
+    parseOptions(newValue: string | OptionType[]) {
+        if (newValue) {
+            typeof newValue === 'string'
+                ? this.radioOptions = JSON.parse(newValue)
+                : this.radioOptions = newValue
+            
+            if (this.radioOptions.length > 0) this.usingSlots = false
+        }
+    }
+
+    // If checked radio changes, emits change event
+    @Watch('defaultValue')
+    handleChange(newValue: string | number) {
+        this.change.emit(newValue)
+    }
+
+    // Controls radios styles and states
+    controlRadio(elements: HTMLBcmRadioElement[]) {
+        elements.forEach((radio: HTMLBcmRadioElement) => {
+            radio.checked = radio.value === this.defaultValue
+
+            radio.optionType = this.optionType
+            radio.buttonStyle = this.buttonStyle
+            radio.size = this.size
+
+            radio.addEventListener('bcm-change', (e: CustomEvent) => {
+                if (e.detail.checked) {
+                    this.defaultValue = radio.value
+                }
+            }, { once: true })
+        })
+    }
+
+    componentWillLoad() {
+        this.parseOptions(this.options)
+    }
+
+    componentDidRender() {
+        let elements: HTMLElement[] = []
+
+        if (this.usingSlots) {
+            elements = this.host.shadowRoot.querySelector('slot').assignedElements() as HTMLElement[]
+            
+            this.childRadios = []
+
+            elements.map(el => {
+                String(el.tagName).toLowerCase() == 'bcm-radio' && this.childRadios.push(el as HTMLBcmRadioElement)
+            })
+
+            this.controlRadio(this.childRadios)
+        }
+
+        else {
+            elements = Array.from(this.host.shadowRoot.querySelectorAll('bcm-radio'))
+
+            this.controlRadio(elements as HTMLBcmRadioElement[])
+        }
+    }
+
+    render() {
+
+        const classes = cs(
+            'radio-group',
+            {
+                [this.direction]: this.optionType === 'default'
+            }
+        )
+
+        if (this.radioOptions) {
+            return (
+                <div class={classes}>
+                    {
+                        this.radioOptions && this.radioOptions.map(radio => (
+                            <bcm-radio value={radio.value} disabled={radio.disabled}> {radio.label} </bcm-radio>
+                        ))
+                    }
+                    <slot />
+                </div>
+            )
+        }
+
+        return (
+            <div class={classes}>
+                <slot />
+            </div>
+        )
+    }
+}
---
+++ src/components/molecules/radio/radio.tsx
@@ -0,0 +1,95 @@
+import { Component, h, Prop, Event, EventEmitter, Host } from "@stencil/core";
+import cs from 'classnames'
+
+@Component({
+    tag: 'bcm-radio',
+    styleUrl: 'radio.scss',
+    shadow: true,
+})
+export class BcmRadio {
+
+    private radio: HTMLInputElement
+
+    @Prop({ mutable: true, reflect: true }) checked: boolean
+    @Prop({ reflect:true }) value: string | number
+    @Prop({ reflect:true }) disabled: boolean
+    @Prop() optionType: 'default' | 'button' = 'default'
+    @Prop() buttonStyle: 'solid' | 'outline' = 'solid'
+    @Prop() size: 'small' | 'medium' | 'large' = 'medium'
+
+    @Event({ eventName: 'bcm-change', bubbles: false }) change: EventEmitter
+    @Event({ eventName: 'bcm-focus', bubbles: false }) focus: EventEmitter
+    @Event({ eventName: 'bcm-blur', bubbles: false }) blur: EventEmitter
+    
+    handleFocus() {
+        this.focus.emit()
+    }
+
+    handleBlur() {
+        this.blur.emit()
+    }
+
+    handleChange() {
+        this.checked = this.radio.checked
+        
+        this.change.emit({
+            value: this.radio.value,
+            checked: this.radio.checked
+        })
+    }
+
+    connectedCallback() {
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    render() {
+
+        const { checked, value, disabled, optionType, buttonStyle, size } = this
+
+        const defaultClasses = cs(
+            'radio-container',
+            'size-2',
+            {
+                'checked': checked,
+                'disabled': disabled
+            }
+        )
+
+        const buttonClasses =cs(
+            'radio-button',
+            buttonStyle,
+            size,
+            {
+                'size-2': size === 'small' || size === 'medium',
+                'size-3': size === 'large',
+                'checked': checked,
+                'disabled': disabled,
+            }
+        )
+
+        return (
+            <Host
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}>
+                <label class={optionType == 'default' ? defaultClasses : buttonClasses}>
+                    <input
+                        type="radio" 
+                        value={value} 
+                        checked={checked}
+                        ref={el => (this.radio = el)}
+                        onChange={this.handleChange}
+                        disabled={disabled}
+                    />
+
+                    {optionType === 'default' && (
+                        <span class="radio">
+                            <span class="inner"></span>
+                        </span>
+                    )}
+
+                    <slot />
+                </label>
+            </Host>
+        )
+    }
+}
