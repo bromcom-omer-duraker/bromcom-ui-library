35d6fccc9b16ae5e4641794ff1acf31cb10679be
OmerDuraker <omer.duraker@bromcom.com>
2021-01-08 19:07:44 +0300

    add switch story

---
+++ src/stories/molecules/switch/switch.stories.js
@@ -0,0 +1,98 @@
+import { h } from 'jsx-dom'
+
+export default {
+    title: 'Components/Molecules/Switch',
+    component: 'bcm-switch',
+    argTypes: {
+        checked: {
+            control: 'boolean',
+            description: 'Default checked state of the switch.',
+            defaultValue: false,
+            table: {
+                category: 'Properties',
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        size: {
+            control: { type: 'select', options: ['small', 'medium'] },
+            description: 'Size of the switch.',
+            defaultValue: 'medium',
+            table: {
+                category: 'Properties',
+                defaultValue: { summary: '"medium"' },
+                type: { summary: 'string' }
+            }
+        },
+        'active-icon': {
+            control: 'text',
+            description: 'Sets active icon name.',
+            defaultValue: 'check',
+            table: {
+                category: 'Properties',
+                type: { summary: 'string' }
+            }
+        },
+        'inactive-icon': {
+            control: 'text',
+            description: 'Sets inactive icon name.',
+            defaultValue: 'close',
+            table: {
+                category: 'Properties',
+                type: { summary: 'string' }
+            }
+        },
+        'active-text': {
+            control: 'text',
+            description: 'Sets active text content.',
+            defaultValue: 'Active',
+            table: {
+                category: 'Properties',
+                type: { summary: 'string' }
+            }
+        },
+        'inactive-text': {
+            control: 'text',
+            description: 'Sets inactive text content.',
+            defaultValue: 'Inactive',
+            table: {
+                category: 'Properties',
+                type: { summary: 'string' }
+            }
+        },
+        disabled: {
+            control: 'boolean',
+            description: 'Set to true to disable the switch.',
+            defaultValue: false,
+            table: {
+                category: 'Properties',
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+        pending: {
+            control: 'boolean',
+            description: 'Sets pending state.',
+            defaultValue: false,
+            table: {
+                category: 'Properties',
+                type: { summary: 'boolean' },
+                defaultValue: { summary: false }
+            }
+        },
+    },
+    parameters: {
+        docs: {
+            description: {
+                component: 'Switching Selector'
+            },
+            source: {
+                code: `<bcm-switch></bcm-switch>`
+            }
+        }
+    }
+}
+
+export const Default = (args) => (
+    <bcm-switch {...args}></bcm-switch>
+)
