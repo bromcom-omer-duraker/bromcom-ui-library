3368c08d8ecc7cd519cbc2570f0292dba5b1407f
Burak Abdikoglu <ahmet@contentwise.info>
2021-01-13 17:15:43 +0300

    Add thumbnail feature for image files

--- src/components/molecules/upload/upload.tsx
+++ src/components/molecules/upload/upload.tsx
@@ -7,6 +7,7 @@ import { Component, h, Element, Host, Prop, State, forceUpdate, Watch, Method } 
 })
 export class BcmUpload {
     private fileInput: HTMLInputElement
+    private imageTypes: string = 'png|jpg|jpeg|gif|bmp'
 
     /**
      * Component Element
@@ -16,7 +17,7 @@ export class BcmUpload {
     /**
      * Component States
      */
-    @State() files: Array<File> = []
+    @State() files: Array<{image: boolean, thumbnail: string, source: File}> = []
     @State() types: Array<string> = []
     
     /**
@@ -83,40 +84,75 @@ export class BcmUpload {
         // to files array if not exist
         Array.from(files).forEach(userFile => {
             let exist: boolean = false
+            let type = userFile.name.split('.').pop()
+            let isImage = this.imageTypes.indexOf(type) !== -1
 
             // Check file type
             // #
-            if (this.types.length > 0 && this.types.indexOf(userFile.name.split('.').pop()) === -1)
+            if (this.types.length > 0 && this.types.indexOf(type) === -1)
                 return;
 
             this.files.map(file => {
                 // Check existance
                 // #
-                file.name === userFile.name && (exist = true)
+                file.source.name === userFile.name && (exist = true)
             })
 
-            !exist && this.files.push(userFile)
+            if (!exist) {
+                const idx = this.files.push({
+                    image: isImage,
+                    source: userFile,
+                    thumbnail: null
+                }) - 1
+
+                isImage && this.createItemThumbnail(idx);
+            }
         })
 
+        // TODO: find alternative way to do this
         forceUpdate(this.el)
     }
 
     /**
      * @desc
+     * @param file 
+     * @param idx 
+     */
+    createItemThumbnail(idx: number) {
+        const reader: FileReader = new FileReader()
+        const file: File = this.files[idx].source
+
+        reader.onloadend = (e: any) => {
+            this.files[idx].thumbnail = 
+                (e.target.result)
+            
+            forceUpdate(this.el)
+        }
+        reader.readAsDataURL(file)
+    }
+
+    /**
+     * @desc
+     * @param idx 
      */
     remove(idx: number) {
         console.log(idx)
     }
-
+    
     @Method()
     async data() {
         const form = new FormData()
-        this.files.map(file => form.append('userFiles[]', file, file.name))
+        this.files.map(file => form.append(
+            'userFiles[]',
+            file.source,
+            file.source.name
+        ))
+        
         return form
     }
 
     render() {
-        const { files } = this
+        const { files, multipleFile } = this
 
         return (
             <Host>
@@ -125,7 +161,7 @@ export class BcmUpload {
                         type="file" 
                         id="file" 
                         class="input"
-                        multiple={this.multipleFile > 1}
+                        multiple={multipleFile > 1}
                         ref={el => this.fileInput = el} 
                         onClick={() => this.handleClick()}
                         onChange={() => this.handleFiles()}
@@ -138,9 +174,17 @@ export class BcmUpload {
 
                 <div class="files">
                     {files.length > 0 && files.map((file, idx) => (
-                        <div class="file">
-                            <bcm-icon icon="paper-clip" size={16} color="grey-7"></bcm-icon>
-                            {file.name}
+                        <div class={'file ' + (file.image ? 'with-thumbnail' : '')}>
+                            { !file.image && (
+                                <bcm-icon icon="paper-clip" size={16} color="grey-7"></bcm-icon>
+                            )}
+                            { file.image && (file.thumbnail && (
+                                <div
+                                    class="thumbnail"
+                                    style={{backgroundImage: 'url('+ file.thumbnail +')'}}>
+                                </div>
+                            ))}
+                            {file.source.name}
                             <bcm-icon
                                 icon="close"
                                 color="grey-10"
