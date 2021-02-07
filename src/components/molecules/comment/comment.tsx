baac5f4372eddfc7047d6db0e8a43c3701d67db1
Burak Abdikoglu <burak.abdikoglu@bromcom.com>
2021-02-07 19:52:37 +0300

    add comment component

---
+++ src/components/molecules/comment/comment.tsx
@@ -0,0 +1,12 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+    tag: 'bcm-comment',
+    styleUrl: 'comment.scss',
+    shadow: true
+})
+export class BcmComment {
+    render() {
+        return ('')
+    }
+}
